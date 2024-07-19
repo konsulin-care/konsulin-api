515a7c61b1aa4974493bc37d41d3b5213c261954
abrahampurnomo <abraham.purnomo@finpoint.id>
2024-07-19 20:06:53 +0700

    feat: add smtp interface & implementation

--- internal/app/drivers/mailer/smtp.go
+++ internal/app/drivers/mailer/smtp.go
@@ -1,1 +1,27 @@
 package mailer
+
+import (
+	"konsulin-service/internal/app/config"
+	"net/smtp"
+
+	"github.com/sirupsen/logrus"
+)
+
+type SMTPClient struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	Auth     smtp.Auth
+}
+
+func NewSMTPClient(driverConfig *config.DriverConfig, log *logrus.Logger) *SMTPClient {
+	auth := smtp.PlainAuth("", driverConfig.SMTP.Username, driverConfig.SMTP.Password, driverConfig.SMTP.Host)
+	return &SMTPClient{
+		Host:     driverConfig.SMTP.Host,
+		Port:     driverConfig.SMTP.Port,
+		Username: driverConfig.SMTP.Username,
+		Password: driverConfig.SMTP.Password,
+		Auth:     auth,
+	}
+}
