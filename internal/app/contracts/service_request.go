9d8f05a89513b65d025c076bdf5a17a5de060545
luckyAkbar <luckyakbar1509@gmail.com>
2025-09-29 09:20:14 +0700

    ensure all necessary group created on application startup

--- internal/app/contracts/service_request.go
+++ internal/app/contracts/service_request.go
@@ -8,4 +8,5 @@ import (
 type ServiceRequestFhirClient interface {
 	CreateServiceRequest(ctx context.Context, request *fhir_dto.CreateServiceRequestInput) (*fhir_dto.CreateServiceRequestOutput, error)
 	GetServiceRequestByIDAndVersion(ctx context.Context, id string, version string) (*fhir_dto.GetServiceRequestOutput, error)
+	EnsureAllNecessaryGroupsExists(ctx context.Context) error
 }
