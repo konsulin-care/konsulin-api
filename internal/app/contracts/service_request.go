593c7f99f232673b5ca74b6aaa181275bcbc4866
luckyAkbar <luckyakbar1509@gmail.com>
2025-11-01 14:37:47 +0700

    feat: new utilities in service request: Search and Update

--- internal/app/contracts/service_request.go
+++ internal/app/contracts/service_request.go
@@ -8,5 +8,7 @@ import (
 type ServiceRequestFhirClient interface {
 	CreateServiceRequest(ctx context.Context, request *fhir_dto.CreateServiceRequestInput) (*fhir_dto.CreateServiceRequestOutput, error)
 	GetServiceRequestByIDAndVersion(ctx context.Context, id string, version string) (*fhir_dto.GetServiceRequestOutput, error)
+	Search(ctx context.Context, input *fhir_dto.SearchServiceRequestInput) ([]fhir_dto.GetServiceRequestOutput, error)
+	Update(ctx context.Context, id string, input *fhir_dto.UpdateServiceRequestInput) (*fhir_dto.GetServiceRequestOutput, error)
 	EnsureAllNecessaryGroupsExists(ctx context.Context) error
 }
