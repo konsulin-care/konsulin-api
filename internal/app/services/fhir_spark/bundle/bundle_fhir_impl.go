7af304f9203e0c8dbb1f5743fc99aba22533f5f9
luckyAkbar <luckyakbar1509@gmail.com>
2025-11-05 07:33:23 +0700

    fix: add proper error handling in Bundle resource interaction to better reflect the error message back to the caller

--- internal/app/services/fhir_spark/bundle/bundle_fhir_impl.go
+++ internal/app/services/fhir_spark/bundle/bundle_fhir_impl.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"konsulin-service/internal/pkg/constvars"
 	"konsulin-service/internal/pkg/exceptions"
 	"konsulin-service/internal/pkg/fhir_dto"
@@ -29,6 +31,7 @@ func NewBundleFhirClient(baseFhirURL string, logger *zap.Logger) *BundleFhirClie
 }
 
 func (c *BundleFhirClientImpl) PostTransactionBundle(ctx context.Context, bundle map[string]any) (*fhir_dto.FHIRBundle, error) {
+	requestID, _ := ctx.Value(constvars.CONTEXT_REQUEST_ID_KEY).(string)
 	requestJSON, err := json.Marshal(bundle)
 	if err != nil {
 		return nil, exceptions.ErrCannotMarshalJSON(err)
@@ -48,7 +51,30 @@ func (c *BundleFhirClientImpl) PostTransactionBundle(ctx context.Context, bundle
 	defer resp.Body.Close()
 
 	if resp.StatusCode != constvars.StatusOK && resp.StatusCode != constvars.StatusCreated {
-		return nil, exceptions.ErrCreateFHIRResource(nil, "Bundle")
+		bodyBytes, rerr := io.ReadAll(resp.Body)
+		if rerr != nil {
+			c.log.Error("bundleFhirClient.PostTransactionBundle error reading response body",
+				zap.String(constvars.LoggingRequestIDKey, requestID),
+				zap.Error(rerr),
+			)
+			return nil, exceptions.ErrCreateFHIRResource(rerr, "Bundle")
+		}
+		var outcome fhir_dto.OperationOutcome
+		if uerr := json.Unmarshal(bodyBytes, &outcome); uerr != nil {
+			c.log.Error("bundleFhirClient.PostTransactionBundle error unmarshaling outcome",
+				zap.String(constvars.LoggingRequestIDKey, requestID),
+				zap.Error(uerr),
+			)
+			return nil, exceptions.ErrCreateFHIRResource(uerr, "Bundle")
+		}
+		if len(outcome.Issue) > 0 {
+			fhirErrorIssue := fmt.Errorf(outcome.Issue[0].Diagnostics)
+			c.log.Error("bundleFhirClient.PostTransactionBundle FHIR error",
+				zap.String(constvars.LoggingRequestIDKey, requestID),
+				zap.Error(fhirErrorIssue),
+			)
+			return nil, exceptions.ErrCreateFHIRResource(fhirErrorIssue, "Bundle")
+		}
 	}
 
 	var result fhir_dto.FHIRBundle
