2e093c017cc4e0b9a7f64379102a3bc11652edc5
luckyAkbar <luckyakbar1509@gmail.com>
2026-01-29 16:43:43 +0700

    misc: constant values for anonymous session related feature

---
+++ internal/pkg/constvars/anonymous_session.go
@@ -0,0 +1,8 @@
+package constvars
+
+const (
+	AnonymousSessionCookieName       = "anon_session"
+	AnonymousSessionGuestIDClaimKey  = "guest_id"
+	AnonymousSessionIdentifierSystem = "https://konsulin/anonymous-guest"
+	AnonymousSessionTokenTTLDays     = 365
+)
