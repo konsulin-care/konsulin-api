ee2346934065c111f0c05b930af927db92479d06
luckyAkbar <luckyakbar1509@gmail.com>
2026-02-01 16:15:36 +0700

    misc: make anonymous token ttl to only 1 day

--- internal/pkg/constvars/anonymous_session.go
+++ internal/pkg/constvars/anonymous_session.go
@@ -1,9 +1,9 @@
 package constvars
 
 const (
-	AnonymousSessionCookieName        = "anon_session"
-	AnonymousSessionForceNewQueryKey  = "force_new"
-	AnonymousSessionGuestIDClaimKey   = "guest_id"
-	AnonymousSessionIdentifierSystem  = "https://login.konsulin.care/guestid"
-	AnonymousSessionTokenTTLDays      = 365
+	AnonymousSessionCookieName       = "anon_session"
+	AnonymousSessionForceNewQueryKey = "force_new"
+	AnonymousSessionGuestIDClaimKey  = "guest_id"
+	AnonymousSessionIdentifierSystem = "https://login.konsulin.care/guestid"
+	AnonymousSessionTokenTTLDays     = 1
 )
